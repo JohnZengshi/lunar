04bee90a0f08bb311db1107d6075da86555bead8
john <johnzengshi@gmail.com>
2023-07-17 13:52:06 +0800

    update

--- lunar.py
+++ lunar.py
@@ -1,7 +1,11 @@
-import json
+'''
+LastEditors: John
+Date: 2023-07-16 13:26:01
+LastEditTime: 2023-07-17 13:46:11
+Author: John
+'''
 import os
 import sys
-import threading
 
 from pynput import keyboard
 from termcolor import colored
@@ -9,43 +13,19 @@ from termcolor import colored
 
 def on_release(key):
     try:
-        if key == keyboard.Key.f1:
+        if key == keyboard.Key.home:
             Aimbot.update_status_aimbot()
-        if key == keyboard.Key.f2:
+        if key == keyboard.Key.end:
             Aimbot.clean_up()
     except NameError:
         pass
 
+
 def main():
     global lunar
-    lunar = Aimbot(collect_data = "collect_data" in sys.argv)
+    lunar = Aimbot(collect_data="collect_data" in sys.argv)
     lunar.start()
 
-def setup():
-    path = "lib/config"
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    print("[INFO] In-game X and Y axis sensitivity should be the same")
-    def prompt(str):
-        valid_input = False
-        while not valid_input:
-            try:
-                number = float(input(str))
-                valid_input = True
-            except ValueError:
-                print("[!] Invalid Input. Make sure to enter only the number (e.g. 6.9)")
-        return number
-
-    xy_sens = prompt("X-Axis and Y-Axis Sensitivity (from in-game settings): ")
-    targeting_sens = prompt("Targeting Sensitivity (from in-game settings): ")
-
-    print("[INFO] Your in-game targeting sensitivity must be the same as your scoping sensitivity")
-    sensitivity_settings = {"xy_sens": xy_sens, "targeting_sens": targeting_sens, "xy_scale": 10/xy_sens, "targeting_scale": 1000/(targeting_sens * xy_sens)}
-
-    with open('lib/config/config.json', 'w') as outfile:
-        json.dump(sensitivity_settings, outfile)
-    print("[INFO] Sensitivity configuration complete")
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -60,11 +40,6 @@ if __name__ == "__main__":
 
     (Neural Network Aimbot)''', "yellow"))
 
-    path_exists = os.path.exists("lib/config/config.json")
-    if not path_exists or ("setup" in sys.argv):
-        if not path_exists:
-            print("[!] Sensitivity configuration is not set")
-        setup()
     path_exists = os.path.exists("lib/data")
     if "collect_data" in sys.argv and not path_exists:
         os.makedirs("lib/data")
